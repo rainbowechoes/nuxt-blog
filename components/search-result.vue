d056c1cfd731b5c0f4473a2b0336764bbdfed587
jer8ome <1762960779@qq.com>
2019-10-29 21:15:53 +0800

    finish search page use to display search result and category、tag result

---
+++ components/search-result.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-card class="box-card" header="Result" shadow="hover">
+    <div v-for="it in result" :key="it" class="text item">
+      <b-row>
+        <b-col cols="8" style="text-align: left;">
+          <nuxt-link :to="it.url">
+            <el-link :underline="false" v-text="it.title">
+            </el-link>
+          </nuxt-link>
+        </b-col>
+        <b-col cols="4" style="text-align: right;" v-text="it.date"></b-col>
+      </b-row>
+      <el-divider class="search-line"></el-divider>
+    </div>
+    <pagination></pagination>
+  </el-card>
+</template>
+
+<script>
+  import Pagination from '~/components/pagination'
+  import {BRow, BCol} from 'bootstrap-vue'
+
+  export default {
+    name: 'search-result',
+    components: {
+      Pagination,
+      BRow,
+      BCol
+    },
+    data () {
+      return {
+        result: [
+          {
+            url: '/',
+            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, magni.',
+            date: '2018年9月5日'
+          },
+          {
+            url: '/',
+            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, magni.',
+            date: '2018年9月5日'
+          },
+          {
+            url: '/',
+            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, magni.',
+            date: '2018年9月5日'
+          },
+          {
+            url: '/',
+            title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, magni.',
+            date: '2018年9月5日'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  /deep/.search-line {
+    margin: 10px 0;
+  }
+</style>
---
+++ pages/search/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <b-row>
+    <!-- 桌面设备显示 -->
+    <b-col lg="8">
+      <search-result></search-result>
+    </b-col>
+    <b-col lg="4" class="d-none d-lg-block d-md-none ">
+      <right></right>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+  import SearchResult from '~/components/search-result'
+  import Right from '~/components/right-bar'
+  import {BRow,BCol} from 'bootstrap-vue'
+  export default {
+    name: "index",
+    components: {
+      Right,
+      BRow,
+      BCol,
+      SearchResult
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
